619d5e5dd179564ca5f4d0ea6dcd552377850417
esmail sarwari <esmail.sarwari10@gmail.com>
2024-06-17 14:31:38 -0700

    Database created and models created - CRUD operation

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Photo = require('./models/Photo');
+
+const port = 3000;
+const app = express();
+// DB Connection
+mongoose.connect('mongodb://localhost/pcat-db');
+
+// template engine
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// routes
+app.get('/', async (req, res) => {
+    const photos = await Photo.find({});
+    res.render('index', {
+        photos
+    });
+});
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+app.get('/add', (req, res) => {
+    res.render('add');
+});
+app.get('*', (req, res) => {
+    res.status(404).send('404 Page Not Found');
+});
+app.post('/photos', async (req, res) => {
+    await Photo.create(req.body);
+    res.redirect('/');
+});
+
+// Express & Port
+app.listen(port, () => {
+    console.log(`server is running on port: ${port}`);
+});
---
+++ models/Photo.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Model = mongoose.model;
+
+async function main() {
+
+    // Create Photo Shcema
+    const photoSchema = new Schema({
+        title: String,
+        description: String,
+        image: String,
+        dateCreated: {
+            type: Date,
+            default: Date.now()
+        }
+    })
+
+    // Create Photo Model/ Create the collection basec on the created schema
+    const Photo = Model('photo', photoSchema);
+    module.exports = Photo;
+}
+main();
