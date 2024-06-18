246abae748c1c354c51493479947a222278de502
esmail sarwari <esmail.sarwari10@gmail.com>
2024-06-18 11:56:12 -0700

    file uploading added

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Photo = require('./models/Photo');
+const fileUpload = require('express-fileupload');
+const fs = require('fs');
+const { dir } = require('console');
 
 const port = 3000;
 const app = express();
@@ -14,6 +17,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 
 // routes
 app.get('/', async (req, res) => {
@@ -37,9 +41,22 @@ app.get('/add', (req, res) => {
 app.get('*', (req, res) => {
     res.status(404).send('404 Page Not Found');
 });
-app.post('/photos', async (req, res) => {
-    await Photo.create(req.body);
-    res.redirect('/');
+app.post('/photos', (req, res) => {
+    const uploadDir = 'public/uploads';
+    if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir);
+    }
+
+    let uploadPhoto = req.files.image;
+    let uploadPath = __dirname + '/public/uploads/' + uploadPhoto.name;
+
+    uploadPhoto.mv(uploadPath, async () => {
+        await Photo.create({
+            ...req.body,
+            image: '/uploads/' + uploadPhoto.name,
+        });
+        res.redirect('/');
+    });
 });
 
 // Express & Port
